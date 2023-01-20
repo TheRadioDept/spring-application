949b9f87f0b12aa1213006ca074832a588b6023f
TheRadioDept <farkhadmaratov99@gmail.com>
2023-01-21 01:42:46 +0600

    initial commit

---
+++ src/main/java/com/netrunner/main_page/MainPageApplication.java
@@ -0,0 +1,12 @@
+package com.netrunner.main_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainPageApplication.class, args);
+	}
+}
---
+++ src/test/java/com/netrunner/main_page/MainPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netrunner.main_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
