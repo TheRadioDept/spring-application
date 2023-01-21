a6d1cdfa884a0d78e49256bb49c6c1fae76f39da
TheRadioDept <farkhadmaratov99@gmail.com>
2023-01-22 01:03:45 +0600

    Added function to store data from Webpage into mysql DB

--- src/main/java/com/netrunner/main_page/models/Post.java
+++ src/main/java/com/netrunner/main_page/models/Post.java
@@ -55,4 +55,12 @@ public class Post {
     public int getViews() {
         return views;
     }
+
+    public Post(){
+    }
+    public Post(String title, String anons, String full_text) {
+        this.title = title;
+        this.anons = anons;
+        this.full_text = full_text;
+    }
 }
